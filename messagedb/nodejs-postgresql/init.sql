d2ad47a3a54293b42c93cba0e9ed5437271d30ab
Kris Gish <kris.gish.jf1v@statefarm.com>
2022-04-14 13:23:51 -0700

    updated msgdb

--- messagedb/nodejs-postgresql/init.sql
+++ messagedb/nodejs-postgresql/init.sql
@@ -1,5 +1,13 @@
-CREATE USER docker1 WITH PASSWORD 'password1';
-CREATE DATABASE messagedb;
-GRANT ALL PRIVILEGES ON DATABASE messagedb TO docker1;
+-- CREATE USER docker1 WITH PASSWORD 'password1';
+-- CREATE DATABASE messagedb;
+-- GRANT ALL PRIVILEGES ON DATABASE messagedb TO docker1;
 
-CREATE TABLE IF NOT EXISTS message(msg_id INT NOT NULL,message_data varchar(250) NOT NULL, PRIMARY KEY (msg_id));
+-- CREATE TABLE IF NOT EXISTS message(msg_id INT NOT NULL,message_data varchar(250) NOT NULL, PRIMARY KEY (msg_id));
+
+CREATE TABLE public.Messages (
+    id SERIAL,
+    timestmp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    userName VARCHAR(255),
+    msg VARCHAR(255)
+);
+CREATE SEQUENCE hibernate_sequence START 1;
